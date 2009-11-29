f8cad35272d021742306f7b273e544305efa9d9f
Daniel Lo Nigro <daniel@dan.cx>
2009-11-29 12:07:27 +0000

    Added missing license text

--- README.txt
+++ README.txt
@@ -11,3 +11,18 @@ Requirements:
  - Visual Studio 2008 (should work in 2005 as well as 2010 beta, but this has not been tested yet)
    - C# and C++ both have to be installed
  - Winamp
+
+License
+=======
+Sharpamp is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Sharpamp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Sharpamp.  If not, see <http://www.gnu.org/licenses/>.
--- Samples/HelloWorldGUI/HelloWorldGUI/HelloWorldGUI/frmTest.cs
+++ Samples/HelloWorldGUI/HelloWorldGUI/HelloWorldGUI/frmTest.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/*
+ * Sharpamp version 0.1 beta
+ * $Id$
+ * Copyright (C) 2009, Daniel Lo Nigro (Daniel15) <daniel at d15.biz>
+ * 
+ * This file is part of Sharpamp.
+ * 
+ * Sharpamp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Sharpamp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Sharpamp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
 using System.Windows.Forms;
 using Daniel15.Sharpamp;
 
